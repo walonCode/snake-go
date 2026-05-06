e7daee4fae920b99f311fa7c0edc60e3aeac0903
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-05-06 22:54:22 +0000

    add the raylib library

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+var allowMove = false
+var green = rl.Color{173, 204, 96, 255}
+var darkGreen = rl.Color{43, 51, 24, 255}
+
+var cellsize = 30
+var cellcount = 25
+var offset = 75
+
+var lastUpdateTime float64 = 0
